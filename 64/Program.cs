1ff1ee21ff8989251914fe23614d186ab639e62c
Sofia Kabanova <sofia.kabanova@gmail.com>
2023-02-26 20:55:13 +0300

    add 64

--- 64/Program.cs
+++ 64/Program.cs
@@ -5,14 +5,11 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 Console.Clear();
-Console.Write("Введите положительное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int m = 1;
-if (n < 1)
+int InputInt(string output)
 {
-    Console.WriteLine("Вы ввели не положительное число.");
+    Console.Write(output);
+    return int.Parse(Console.ReadLine()!);
 }
-Console.WriteLine(NaturalNumber(n, m));
 int NaturalNumber(int n, int m)
 {
     if (n == m)
@@ -21,5 +18,14 @@ int NaturalNumber(int n, int m)
         Console.Write($"{NaturalNumber(n, m + 1)}, ");
     return m;
 }
-
-
+int n = InputInt("Введите положительное число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число. Перезапустите программу.");
+}
+else
+{
+    Console.WriteLine($"Все натуральные числа в промежутке от {n} до 1: ");
+    Console.WriteLine(NaturalNumber(n, m));
+}
