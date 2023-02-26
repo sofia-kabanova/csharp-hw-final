6f4f5e7c99b5fcb8b4d713a71790e3e72132fa2c
Sofia Kabanova <sofia.kabanova@gmail.com>
2023-02-26 20:38:38 +0300

    add 68

---
+++ 68/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 68:
+// Напишите программу
+// вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Clear();
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine()!);
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+if (m < 0 & n >= 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число.");
+    Console.WriteLine("Перезапустите программу и повторите ввод");
+}
+else if (m >= 0 & n < 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число.");
+    Console.WriteLine("Перезапустите программу и повторите ввод");
+}
+else if (m < 0 & n < 0)
+{
+    Console.WriteLine("Вы ввели отрицательные числа.");
+    Console.WriteLine("Перезапустите программу и повторите ввод");
+}
+else
+{
+    Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+}
